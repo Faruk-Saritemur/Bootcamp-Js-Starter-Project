b88f953730a655306908e148785a27a37c43b1ec
Faruk Sarıtemur <89876428+Faruk-Saritemur@users.noreply.github.com>
2021-10-20 00:49:51 +0300

    20 Ekim

---
+++ src/app.js
@@ -0,0 +1,7 @@
+
+console.log("slm");
+let konutKredisi = ["Konut kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi"];
+
+for(let i=0; i<konutKredisi.length;i++) {
+    console.log(konutKredisi[i]);
+}
